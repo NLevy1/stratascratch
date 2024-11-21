5d1a268309706f43eb639ee115a41b055394d69c
Nathan Levy <B07383@launcher.int.abnamro.com>
2024-11-21 16:01:26 +0100

    Add code + tests for bikes_last_used

--- src/bikes_last_used.py
+++ src/bikes_last_used.py
@@ -1,21 +1,33 @@
 from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, to_timestamp
+from pyspark.sql.functions import max as fmax
+
+FINAL_COLUMN_ORDER = [
+    "end_time",
+    "bike_number",
+]
 
 
-def run_script(input_df: DataFrame):
+def clean_input_df(input_df: DataFrame) -> DataFrame:
+    """_summary_
+
+    Args:
+        input_df (DataFrame): _description_
+
+    Returns:
+        DataFrame: _description_
+    """
+    return input_df.withColumn("end_time", to_timestamp("end_time", "dd-M-yyyy H:mm"))
+
+
+def groupby_max_end_time(input_df: DataFrame) -> DataFrame:
+    """_summary_"""
+    return input_df.groupby("bike_number").agg(fmax("end_time").alias("end_time")).sort(col("end_time").desc())
+
+
+def get_bikes_last_used(input_df: DataFrame) -> DataFrame:
     """_summary_"""
 
-    columns_to_select_bikes = [
-        "duration",
-        "duration_seconds",
-        "start_time",
-        "start_station",
-        "start_terminal",
-        "end_time",
-        "end_station",
-        "end_terminal",
-        "bike_number",
-        "rider_type",
-        "id",
-    ]
-
-    return input_df.select(columns_to_select_bikes)
+    cleaned_input_df = clean_input_df(input_df)
+    x = groupby_max_end_time(cleaned_input_df)
+    return x.select(FINAL_COLUMN_ORDER)
--- tests/test_bikes_last_used.py
+++ tests/test_bikes_last_used.py
@@ -1,12 +1,46 @@
+from datetime import datetime
+
 import pytest
+from chispa import assert_df_equality
+from pyspark.sql.types import StringType, StructField, StructType, TimestampType
+
+from src.bikes_last_used import get_bikes_last_used
 
 
-@pytest.fixture
-def sample_df(spark_session):
-    data = []
-    schema = []
+@pytest.fixture()
+def input_df(spark_session):
+    data = [
+        ("25-3-2012 10:40", "W00576"),
+        ("28-3-2012 19:11", "W00576"),
+        ("12-3-2012 22:37", "W01215"),
+        ("30-3-2012 20:00", "W01215"),
+        ("12-3-2012 20:15", "W00455"),
+    ]
+    schema = StructType(
+        [
+            StructField("end_time", StringType(), True),
+            StructField("bike_number", StringType(), True),
+        ]
+    )
     return spark_session.createDataFrame(data, schema)
 
 
-def test_dummy():
-    assert True
+@pytest.fixture()
+def expected_df(spark_session):
+    data = [
+        (datetime(2012, 3, 30, 20, 00), "W01215"),
+        (datetime(2012, 3, 28, 19, 11), "W00576"),
+        (datetime(2012, 3, 12, 20, 15), "W00455"),
+    ]
+    schema = StructType(
+        [
+            StructField("end_time", TimestampType(), True),
+            StructField("bike_number", StringType(), True),
+        ]
+    )
+    return spark_session.createDataFrame(data, schema)
+
+
+def test_groupby_bikes(input_df, expected_df):
+    actual_df = get_bikes_last_used(input_df)
+    assert_df_equality(actual_df, expected_df)
